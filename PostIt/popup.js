8917c8e2236d9efb2156c3a5f252d865cc90809c
Léa BARBEAU <leabarbeau@yahoo.fr>
2018-04-10 12:09:39 +0200

    Plugin Chrome PostIt
    Création d'une extension chrome de PostIn simple.

---
+++ PostIt/popup.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+	var txt = document.getElementById("txt");
+	txt.value = localStorage.getItem("txt");
+
+	var button = document.getElementById("btn");
+	button.addEventListener("click", saveData);
+
+	function saveData(){
+		localStorage.setItem("txt", txt.value);
+	}
+}
