d140e304fad4a1f07dab8b12088701ac962facf3
Léa BARBEAU <leabarbeau@yahoo.fr>
2018-04-10 12:12:17 +0200

    Application cocktails Angular
    Création d'une application angular dynamique d'édition de cocktails.

---
+++ cocktails/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Cocktail } from './shared/models/cocktail.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+
+  //cocktail = new Cocktail("mojito");
+}
+ 
---
+++ cocktails/src/app/cocktail-container/cocktails-details/cocktails-details.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Cocktail } from '../../shared/models/cocktail.model';
+import { Ingredient } from '../../shared/models/ingredient.model';
+import { CocktailService } from '../../shared/services/cocktail.service';
+import { PanierService } from '../../shared/services/panier.service';
+
+@Component({
+  selector: 'app-cocktails-details',
+  templateUrl: './cocktails-details.component.html',
+  styleUrls: ['./cocktails-details.component.css']
+})
+export class CocktailsDetailsComponent implements OnInit {
+  public cocktail: Cocktail;
+  public index: number;
+
+  constructor(private activatedRoute: ActivatedRoute, private cocktailService: CocktailService, private panierService: PanierService) { }
+
+  ngOnInit() {
+    this.activatedRoute.params.subscribe((params: Params) => {
+      if (params.index){
+        this.index = params.index;   
+      }else {
+        this.index = 0;
+      }  
+      this.cocktailService.getCocktail(this.index).subscribe((cocktail: Cocktail) => {
+        this.cocktail = cocktail;
+      });  
+    })
+  }
+
+  addPanier(ingredients: Ingredient[]): void{
+    this.panierService.addIngredients(ingredients);
+  }
+  
+  getUrl(): string[]{
+    return ['/cocktails', this.index + '', 'edit']
+  }
+}
